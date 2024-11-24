1fc45151527c774cd9864b00158b23a728b1ea3d
Robert (Jamie) Munro <rjmunro@arjam.net>
2024-11-24 22:36:46 +0000

    Add query client

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,19 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+});
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
