1fc09d653dec64918339069ab8ab2b12b470a670
Robert (Jamie) Munro <rjmunro@arjam.net>
2024-11-25 11:40:24 +0000

    Extract location query to it’s own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
 import { useState } from "react";
-import { QueryClient, useQuery } from "react-query";
 
 import "./App.css";
 import { LocationForm } from "./components/LocationForm";
 import { WeatherComponent } from "./components/WeatherComponent";
-
-interface LocationResult {
-  id: number;
-  name: string;
-  latitude: number;
-  longitude: number;
-  elevation: number;
-  feature_code: string;
-  country_code: string;
-  admin1_id: number;
-  admin2_id?: number; // Admins apart from first are optional
-  admin3_id?: number;
-  admin4_id?: number;
-  timezone: string;
-  population: number;
-  postcodes: string[];
-  country_id: number;
-  country: string;
-  admin1: string;
-  admin2?: string; // Admins apart from first are optional
-  admin3?: string;
-  admin4?: string;
-}
+import { LocationResult, useLocationQuery } from "./queries/LocationQuery";
 
 function joinAdminNames(row: LocationResult): [string] {
   return [row.admin1, row.admin2, row.admin3, row.admin4].filter(
@@ -42,25 +19,7 @@ function App() {
     isLoading: searchLoading,
     error: searchError,
     data: searchData,
-  } = useQuery<LocationResult[]>(
-    ["locationSearch", searchTerm],
-
-    async () => {
-      if (searchTerm === "") {
-        return [];
-      }
-      const url = new URL(
-        "https://geocoding-api.open-meteo.com/v1/search?count=10&language=en&format=json"
-      );
-      url.searchParams.set("name", searchTerm);
-      const res = await fetch(url);
-      if (res.status == 200) {
-        return (await res.json()).results;
-      } else {
-        throw new Error(await res.text());
-      }
-    }
-  );
+  } = useLocationQuery(searchTerm);
 
   return (
     <>
---
+++ src/queries/LocationQuery.tsx
@@ -0,0 +1,46 @@
+import { useQuery } from "react-query";
+
+export interface LocationResult {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1_id: number;
+  admin2_id?: number; // Admins apart from first are optional
+  admin3_id?: number;
+  admin4_id?: number;
+  timezone: string;
+  population: number;
+  postcodes: string[];
+  country_id: number;
+  country: string;
+  admin1: string;
+  admin2?: string; // Admins apart from first are optional
+  admin3?: string;
+  admin4?: string;
+}
+
+export function useLocationQuery(searchTerm: string) {
+  return useQuery<LocationResult[]>(
+    ["locationSearch", searchTerm],
+
+    async () => {
+      if (searchTerm === "") {
+        return [];
+      }
+      const url = new URL(
+        "https://geocoding-api.open-meteo.com/v1/search?count=10&language=en&format=json"
+      );
+      url.searchParams.set("name", searchTerm);
+      const res = await fetch(url);
+      if (res.status == 200) {
+        return (await res.json()).results;
+      } else {
+        throw new Error(await res.text());
+      }
+    }
+  );
+}
