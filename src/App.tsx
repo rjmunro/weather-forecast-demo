7e7cd42356f6f77ea055aba934794529f11a7cec
Robert (Jamie) Munro <rjmunro@arjam.net>
2024-11-25 14:31:43 +0000

    Hide results list when one is chosen

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,27 @@ import "./App.css";
 function App() {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [location, setLocation] = useState<LocationResult>();
+  const [showResults, setShowResults] = useState(true);
 
   return (
     <>
-      <LocationForm value={searchTerm} setValue={setSearchTerm} />
-      <LocationResults
-        searchTerm={searchTerm}
-        location={location}
-        setLocation={setLocation}
+      <LocationForm
+        value={searchTerm}
+        setValue={(searchTerm) => {
+          setSearchTerm(searchTerm);
+          setShowResults(true);
+        }}
       />
+      {showResults ? (
+        <LocationResults
+          searchTerm={searchTerm}
+          location={location}
+          setLocation={(location) => {
+            setLocation(location);
+            setShowResults(false);
+          }}
+        />
+      ) : null}
       {location ? (
         <>
           <h2>
